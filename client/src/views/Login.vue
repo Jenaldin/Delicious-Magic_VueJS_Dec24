4725cfccb397b3183aa1137708c3a992c1a6b113
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-12-31 04:43:32 +0200

    Added configuration and pages for routing

---
+++ client/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>
+     <h2>Login</h2>
+     <!-- Login form here -->
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+   name: 'Login',
+ };
+ </script>
+ 
---
+++ client/src/views/Register.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>
+     <h2>Register</h2>
+     <!-- Registration form here -->
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+   name: 'Register',
+ };
+ </script>
+ 
