e7ede505b38e1adeb9480112f03173129578ccd5
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-12-31 16:35:19 +0200

    Adjusting colors and adding a theme link

--- client/src/main.js
+++ client/src/main.js
@@ -7,11 +7,22 @@ import pinia from './stores';
 import axiosApi from './axios';
 import { useVuelidate } from '@vuelidate/core';
 
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
 const app = createApp(App);
 app.config.globalProperties.$axios = axiosApi;
+
+const vuetify = createVuetify({
+   components,
+   directives,
+});
 
 app.use(pinia);
 app.use(router);
 app.use(useVuelidate);
+app.use(vuetify);
 
 app.mount('#app');
