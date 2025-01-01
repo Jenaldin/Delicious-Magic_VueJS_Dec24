c9782ee2bf19b5888ed7ab449f3485b9d5d2c4b3
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-02 00:43:44 +0200

    Added Footer.vue component

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+   name: 'Footer',
+};
+</script>
+
+<template>
+   <v-footer>
+      <v-col class="text-center">
+         © {{ new Date().getFullYear() }} Delicious Magic Recipes, Jenny Guteva, SoftUni VueJS Exam Project
+      </v-col>
+   </v-footer>
+</template>
