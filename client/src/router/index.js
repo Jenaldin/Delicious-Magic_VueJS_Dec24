bced54ae6671fd4ec91719004aab5ce47dfe1ea1
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-02 04:56:20 +0200

    Renamed View folder to Pages

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Register from '../views/Register.vue';
-import Login from '../views/Login.vue';
+import Home from '../pages/Home.vue';
+import Register from '../pages/Register.vue';
+import Login from '../pages/Login.vue';
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/register', name: 'register', component: Register},
   { path: '/login', name: 'login', component: Login},
   { path: '/about', name: 'about',
-    component: () => import('../views/About.vue'),
+    component: () => import('../pages/About.vue'),
   },
 ];
 
