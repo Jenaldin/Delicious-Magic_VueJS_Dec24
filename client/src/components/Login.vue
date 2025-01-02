ac75f1b972b99f371dcb44c040d74b5ce96908b9
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-02 06:20:06 +0200

    Created Login.vue and Register.vue components with placeholder content

---
+++ client/src/components/Login.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+   <v-form>
+     <v-text-field label="Email" required></v-text-field>
+     <v-text-field label="Password" type="password" required></v-text-field>
+     <v-btn type="submit" color="amber-darken-1">Login</v-btn>
+   </v-form>
+ </template>
+ 
+ <style scoped>
+ </style>
---
+++ client/src/components/Register.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+   <v-form>
+     <!-- Registration form fields and submit button -->
+     <v-text-field label="Email" required></v-text-field>
+     <v-text-field label="Password" type="password" required></v-text-field>
+     <v-text-field label="Confirm Password" type="password" required></v-text-field>
+     <v-btn type="submit" color="amber-darken-1">Register</v-btn>
+   </v-form>
+ </template>
+
+ <style scoped>
+ </style>
+ 
+ 
