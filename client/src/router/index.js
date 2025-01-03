d45d97c62d5759976592c2ee41f175957c42a0da
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-03 02:32:01 +0200

    Added a guard to router and made home lazy loaded

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,18 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../pages/Home.vue';
 import Entry from '../pages/Entry.vue';
+import { useAuthStore } from '../stores/auth';
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/register-login', name: 'register-login', component: Entry},
-  { path: '/about', name: 'about',
-    component: () => import('../pages/About.vue'),
-  },
+  { path: '/', name: 'home', component: () => import('../pages/Home.vue') },
+  { path: '/about', name: 'about', component: () => import('../pages/About.vue') },
+  { path: '/register-login', name: 'register-login', component: Entry },
 ];
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => { 
+  const authStore = useAuthStore(); 
+  if (to.name === 'register-login' && authStore.isAuthenticated) { 
+    next({ name: 'home' }); 
+  } else { 
+    next(); 
+  }
 });
 
 export default router;
