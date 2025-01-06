93d57cbd07dc30a1c09d8b39d2ddeb6957b298e7
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-06 03:50:42 +0200

    Made api calls to provide filter

--- client/src/api/recipes.js
+++ client/src/api/recipes.js
@@ -1,8 +1,8 @@
 import axiosApi from '../axios.js'
 
-export const getAllRecipes = async (pageNumber, pageSize) => {
+export const getAllRecipes = async (type, pageNumber, pageSize) => {
   try {
-    const response = await axiosApi.get('/catalog', { params: { pageNumber, pageSize } })
+    const response = await axiosApi.get('/catalog', { params: { type, pageNumber, pageSize } })
     return response.data
   } catch (error) {
     throw new Error('Error fetching recipes: ' + error.message)
