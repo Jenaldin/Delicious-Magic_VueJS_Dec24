61bd923ba9579080cb3b47b632cd35be52eee90c
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-06 04:53:34 +0200

    Created components for View, New and Edit recipes

---
+++ client/src/components/EditRecipe.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+   <h1>Edit Recipe Scroll</h1>
+</template>
+
+<style scoped>
+</style>
---
+++ client/src/components/NewRecipe.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+   <h1>New Recipe Scroll</h1>
+</template>
+
+<style scoped>
+</style>
