f329749fb85069cdf48c06d8a2e50e101a9b5fe2
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-06 18:48:11 +0200

    Renamed pages for better readability

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { useAuthStore } from '../stores/authStore';
-import Entry from '../pages/Entry.vue';
-import Catalog from '../pages/Catalog.vue';
-import Details from '../pages/Details.vue';
+import Entry from '../pages/EntryPage.vue';
+import Catalog from '../pages/CatalogPage.vue';
+import Details from '../pages/DetailsPage.vue';
 
 const routes = [
-  { path: '/', name: 'home', component: () => import('../pages/Home.vue') },
-  { path: '/about', name: 'about', component: () => import('../pages/About.vue') },
+  { path: '/', name: 'home', component: () => import('../pages/HomePage.vue') },
+  { path: '/about', name: 'about', component: () => import('../pages/AboutPage.vue') },
   { path: '/register-login', name: 'register-login', component: Entry },
   { path: '/catalog', name: 'catalog', component: Catalog },
   { path: '/add-recipe', name: 'add-recipe', component: Details, meta: { requiresAuth: true } },
   { path: '/view-recipe/:id', name: 'view-recipe', component: Details },
   { path: '/edit-recipe/:id', name: 'edit-recipe', component: Details, meta: { requiresAuth: true } },
-  { path: '/:pathMatch(.*)*', name: '404', component: () => import('../pages/PageNotFound.vue') },
+  { path: '/:pathMatch(.*)*', name: '404', component: () => import('../pages/PageNotFoundPage.vue') },
 ];
 
 const router = createRouter({
