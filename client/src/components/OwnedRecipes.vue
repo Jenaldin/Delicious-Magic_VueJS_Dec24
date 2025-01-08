1f3bee06396cbeb75506e2643332769ddce55a87
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-08 03:37:03 +0200

    Refactored Owned Recipes to work

--- client/src/components/OwnedRecipes.vue
+++ client/src/components/OwnedRecipes.vue
@@ -1,74 +1,102 @@
 <script setup>
-</script>
-
-<template>
-   <p>Owned here</p>
-</template>
-
-<!-- <script setup>
-import { ref, onMounted } from 'vue';
-import { getUserRecipes } from '../api/userApi';
+import { ref, computed, onMounted } from 'vue';
+import { getUser } from '../api/authUserApi';
 import { deleteRecipe } from '../api/recipeApi';
 import { useRouter } from 'vue-router';
+import { useAuthStore } from '../stores/authStore';
 
-const props = defineProps({ userId: String });
+const props = defineProps({
+  userId: {
+    type: String,
+    required: true,
+  },
+});
+
 const router = useRouter();
+const auth = useAuthStore();
+const isOwner = computed(() => auth.userId === props.userId);
 
-const recipesOwned = ref([]);
+const recipesOwned = ref([])
 const snackbar = ref({
   show: false,
   message: '',
-  color: 'green-darken-4'
-});
+  color: 'green-darken-4',
+})
 
-const fetchRecipes = async () => {
+const fetchRecipesFromUser = async () => {
   try {
-    recipesOwned.value = await getUserRecipes(props.userId);
+    const user = await getUser(props.userId)
+    recipesOwned.value = user.recipesOwned
   } catch (error) {
     snackbar.value = {
       show: true,
       message: error.response?.data?.error || error.message,
-      color: 'red-darken-4'
-    };
+      color: 'red-darken-4',
+    }
   }
-};
+}
 
 onMounted(() => {
-  fetchRecipes();
+  fetchRecipesFromUser()
 });
 
 const del = async (recipeId) => {
   try {
-    await deleteRecipe(recipeId);
-    snackbar.value.message = 'Recipe deleted successfully!';
-    snackbar.value.color = 'green-darken-4';
-    snackbar.value.show = true;
-    await fetchRecipes();
+    await deleteRecipe(recipeId)
+    snackbar.value.message = 'Recipe deleted successfully!'
+    snackbar.value.color = 'green-darken-4'
+    snackbar.value.show = true
+    await fetchRecipesFromUser()
   } catch (error) {
-    snackbar.value.message = error.response?.data?.error || error.message;
-    snackbar.value.color = 'red-darken-4';
-    snackbar.value.show = true;
+    snackbar.value.message = error.response?.data?.error || error.message
+    snackbar.value.color = 'red-darken-4'
+    snackbar.value.show = true
   }
-};
+}
 </script>
 
 <template>
   <div>
     <v-card v-for="recipe in recipesOwned" :key="recipe._id">
-      <v-card-title>{{ recipe.title }}</v-card-title>
-      <v-card-subtitle>Type: {{ recipe.type }}</v-card-subtitle>
-      <v-card-actions>
-        <v-btn color="green-darken-4" @click="() => router.push({ name: 'edit-recipe', params: { id: recipe._id } })">Edit</v-btn>
-        <v-btn color="red-darken-4" @click="() => del(recipe._id)">Delete</v-btn>
-      </v-card-actions>
+      <v-row
+        ><v-col>
+          <v-img
+            :src="recipe.image ? recipe.image : '/img-placeholder.png'"
+            alt="Recipe Image"
+            max-height="65"
+            max-width="65"
+          ></v-img
+        ></v-col>
+        <v-col
+          ><v-card-title>{{ recipe.title }}</v-card-title></v-col
+        >
+        <v-col
+          ><v-card-subtitle>Type: {{ recipe.type }}</v-card-subtitle></v-col
+        >
+        <div v-if="isOwner">
+        <v-col>
+          <v-card-actions>
+            <v-btn
+              color="green-darken-4" 
+              variant="tonal"
+              @click="() => router.push({ name: 'edit-recipe', params: { id: recipe._id } })"
+              >Edit</v-btn
+            >
+            <v-btn color="red-darken-4" variant="tonal" @click="() => del(recipe._id)">Delete</v-btn>
+          </v-card-actions></v-col
+        ></div>
+      </v-row>
     </v-card>
     <v-snackbar v-model="snackbar.show" :color="snackbar.color">
       {{ snackbar.message }}
     </v-snackbar>
   </div>
-</template> -->
+</template>
 
 <style scoped>
+.v-card {
+   margin-bottom: 0.5rem;
+}
 .v-btn {
   margin-right: 0.5rem;
 }
