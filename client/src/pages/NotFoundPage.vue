e2d7d8cc57b3f0c8f3e6cd24ac9c29df4e83c7d6
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-08 06:14:36 +0200

    Renamed PageNotFoundPage to NotFoundPage and adjusted its text

--- client/src/pages/NotFoundPage.vue
+++ client/src/pages/NotFoundPage.vue
@@ -10,8 +10,8 @@
     </div>
     <div class="centered-content">
       <p>We tried to show the page you navigated to, but it does not exist or you do not have permissions to see it. </p>
-      <p>Check the link you tried to follow as it might be broken, or the page might have been removed, or it might not exist yet.</p>
-      <h4>For best results, go back to the <router-link to="/">Home page</router-link> and go from there.</h4>
+      <p>Check the link you tried to follow as it might be broken, or the page might have been removed/restricted, or it might not exist yet.</p>
+      <h4>For best results, go back to the <router-link to="/">Home page</router-link> and start from there.</h4>
       <h4>Thank you for visiting!</h4>
     </div>
   </main>
