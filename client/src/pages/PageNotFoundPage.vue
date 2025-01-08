0e9c7f1103b8a7e7deaec73d2ef5931fc69d45c8
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-08 06:08:52 +0200

    Changed wording of NotFound page

--- client/src/pages/PageNotFoundPage.vue
+++ client/src/pages/PageNotFoundPage.vue
@@ -6,13 +6,12 @@
     <div class="centered-content">
       <h1>Oops! Dark Magics!</h1>
       <h3>The recipe scroll you are looking for has vanished!</h3>
+      <h3>(Or you are not authorized to use it...)</h3>
     </div>
     <div class="centered-content">
-      <p>We tried to find the page you navigated to, but it does not exist. </p>
-      <p>The link you tried to follow might be broken, the page might have been removed or might not exist yet.</p>
-      <p>Try navigating back <router-link to="/">Home</router-link> and go from there.</p>
-    </div>
-    <div class="centered-content">
+      <p>We tried to show the page you navigated to, but it does not exist or you do not have permissions to see it. </p>
+      <p>Check the link you tried to follow as it might be broken, or the page might have been removed, or it might not exist yet.</p>
+      <h4>For best results, go back to the <router-link to="/">Home page</router-link> and go from there.</h4>
       <h4>Thank you for visiting!</h4>
     </div>
   </main>
