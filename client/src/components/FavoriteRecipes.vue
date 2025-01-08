a0d809081531f4f299388f191f11ca8d05f90fbc
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2025-01-08 06:43:48 +0200

    Refactored the FavoriteRecipe.vue component

--- client/src/components/FavoriteRecipes.vue
+++ client/src/components/FavoriteRecipes.vue
@@ -1,51 +1,105 @@
 <script setup>
-</script>
+import { ref, onMounted } from 'vue'
+import { getUser } from '../api/authUserApi'
+import { deleteRecipe } from '../api/recipeApi'
+import { useRouter } from 'vue-router'
 
-<template>
-   <p>Favorites here</p>
-</template>
-
-<!-- <script setup>
-import { ref, onMounted } from 'vue';
-import { getFavoriteRecipes } from '../api/userApi';
-
-const props = defineProps({ userId: String });
-
-const recipesFavorite = ref([]);
+const props = defineProps({
+  userId: {
+    type: String,
+    required: true,
+  },
+})
+const router = useRouter()
+const recipesFav = ref([])
 const snackbar = ref({
   show: false,
   message: '',
-  color: 'green-darken-4'
-});
+  color: 'green-darken-4',
+})
 
-const fetchRecipes = async () => {
+const fetchRecipesFromUser = async () => {
   try {
-    recipesFavorite.value = await getFavoriteRecipes(props.userId);
+    const user = await getUser(props.userId)
+    recipesFav.value = user.favorites.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
   } catch (error) {
     snackbar.value = {
       show: true,
       message: error.response?.data?.error || error.message,
-      color: 'red-darken-4'
-    };
+      color: 'red-darken-4',
+    }
   }
-};
+}
 
 onMounted(() => {
-  fetchRecipes();
-});
-</script> 
+  fetchRecipesFromUser()
+})
+
+const del = async (recipeId) => {
+  try {
+    await deleteRecipe(recipeId)
+    snackbar.value.message = 'Recipe deleted successfully!'
+    snackbar.value.color = 'green-darken-4'
+    snackbar.value.show = true
+    await fetchRecipesFromUser()
+  } catch (error) {
+    snackbar.value.message = error.response?.data?.error || error.message
+    snackbar.value.color = 'red-darken-4'
+    snackbar.value.show = true
+  }
+}
+</script>
 
 <template>
-  <div>
-    <v-card v-for="recipe in recipesFavorite" :key="recipe._id">
-      <v-card-title>{{ recipe.title }}</v-card-title>
-      <v-card-subtitle>Type: {{ recipe.type }}</v-card-subtitle>
-    </v-card>
-    <v-snackbar v-model="snackbar.show" :color="snackbar.color">
-      {{ snackbar.message }}
-    </v-snackbar>
-  </div>
-</template> -->
+   <div>
+     <v-card v-for="recipe in recipesFav" :key="recipe._id" class="pa-3">
+       <v-row class="align-center">
+         <v-col cols="auto" class="d-flex align-center">
+           <v-img
+             :src="recipe.image ? recipe.image : '/img-placeholder.png'"
+             alt="Recipe Image"
+             height="65"
+             width="65"
+           ></v-img>
+         </v-col>
+         <v-col cols="auto" class="d-flex align-center">
+           <v-card-title>
+             <h5 class="title-ellipsis">{{ recipe.title }}</h5>
+           </v-card-title>
+         </v-col>
+         <v-col cols="auto" class="d-flex align-center">
+           <v-card-subtitle>
+             <h4>Type: {{ recipe.type }}</h4>
+           </v-card-subtitle>
+         </v-col>
+         <v-col cols="auto" class="d-flex align-center">
+           <v-card-actions>
+             <v-btn
+               color="amber-darken-1"
+               variant="tonal"
+               @click="() => router.push({ name: 'view-recipe', params: { id: recipe._id } })"
+             >View</v-btn>
+           </v-card-actions>
+         </v-col>
+       </v-row>
+     </v-card>
+     <v-snackbar v-model="snackbar.show" :color="snackbar.color">
+       {{ snackbar.message }}
+     </v-snackbar>
+   </div>
+ </template>
+ 
 
 <style scoped>
+.v-card {
+  margin-bottom: 0.5rem;
+}
+
+.title-ellipsis {
+  width: 30ch;
+  white-space: normal;
+}
+.v-btn {
+  margin-right: 0.05rem;
+}
 </style>
